ba43f70fcbf1dfc6b824bd98082fea5c4c0b966d
pedroEEP <pemilioh@gmail.com>
2025-03-04 12:55:07 +0100

    subido nuevo proyecto

---
+++ EjemploPooCoche/EjemploPooCoche/Coche.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploPooCoche
+{
+    internal class Coche
+    {
+    
+        //Atributos
+        public string marca;
+        public string modelo;
+        public string color;
+        public int velocidad;
+        public int agno;
+        public bool encendido;
+
+        //Métodos
+        public void Arrancar()
+        {
+            encendido = true;
+        }
+        public void Parar()
+        {
+            encendido = false;
+        }
+
+        public int VelocidadActual()
+        {
+            return velocidad;
+        }
+    }
+}
+
